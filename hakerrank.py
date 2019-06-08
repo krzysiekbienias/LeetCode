94636943a0aaf34e063352e8f8349ce979483c27
Krzysiek Bienias <krzysiek.bienias.gmail.com>
2019-06-08 13:16:51 +0200

    leetcode problems that i managed to solve

--- hakerrank.py
+++ hakerrank.py
@@ -37,13 +37,42 @@ def compareTriplets(a, b):
     return [a_points, b_points]
 
 
-#Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+
+#HackerLand University has the following grading policy:
+#Every student receives a grade in the inclusive range from 0 to 100.
+#Any  less than 40 is a failing grade.
+
+#Sam is a professor at the university and likes to round each student's  according to these rules:
+#If the difference between the grade and the next multiple of 5 is less than 3, round  up to the next multiple of .
+#If the value of grade is less than 40, no rounding occurs as the result will still be a failing grade.
 
 
+def gradingStudents(grades):
+    adjusted_grades=[]
+
+    for i in range(len(grades)):
+        if grades[i]<38:
+            adjusted_grades.append(grades[i])
+        if (grades[i]>=38):
+            reminder5 = grades[i] % 5
+            if reminder5>=3:
+                grades[i]+=5-reminder5
+                adjusted_grades.append(grades[i])
+            else:
+                adjusted_grades.append(grades[i])
+    return adjusted_grades
+
+
+#leap yeat
 ans_is_leap=is_leap(year=2100)
 
+#cpmpare triples
 a=[5,6,7]
 b=[3,6,10]
 triplas_compare=compareTriplets(a,b)
+
+#rounding grades
+l_grade=[73,67,38,33]
+grade_answ=gradingStudents(grades=l_grade)
 
 print("The End")
