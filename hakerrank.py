fc1a8788245231c8dead3eecf0c0166835f28481
Krzysiek Bienias <krzysiek.bienias.gmail.com>
2019-06-08 15:06:53 +0200

    leetcode problems that i managed to solve

--- hakerrank.py
+++ hakerrank.py
@@ -62,17 +62,52 @@ def gradingStudents(grades):
                 adjusted_grades.append(grades[i])
     return adjusted_grades
 
+# find the image of point p when we translate it by 180 degrees with respect to p
+
+#def image_of_point(ll_p,ll_q):
+#   for i in range(len(ll_p))
+
+
+#Gary is an avid hiker. He tracks his hikes meticulously, paying close attention to small details like topography.
+# During his last hike he took exactly n steps. For every step he took, he noted if it was an uphill, U, or a downhill, D
+# step. Gary's hikes start and end at sea level and each step up or down represents a  unit change in altitude.
+# We define the following terms:
+# 1.A mountain is a sequence of consecutive steps above sea level, starting with a step up from sea level and ending
+# with a step down to sea level.
+# 2.A valley is a sequence of consecutive steps below sea level, starting with a step down from sea level and ending
+# with a step up to sea level.
+
+def countingValleys(s):
+    num_of_valeys=0
+    temp=0
+    for i in range(len(s)):
+        if s[i]=="U":
+            temp+=1
+        if s[i]=="D":
+            temp=-1
+        if temp<-1:
+            num_of_valeys+=1
+    return  num_of_valeys
+
+###############################----SOLUTIONS----###################################################
 
 #leap yeat
 ans_is_leap=is_leap(year=2100)
+
+####################################################################################################
 
 #cpmpare triples
 a=[5,6,7]
 b=[3,6,10]
 triplas_compare=compareTriplets(a,b)
+####################################################################################################
 
 #rounding grades
 l_grade=[73,67,38,33]
 grade_answ=gradingStudents(grades=l_grade)
+####################################################################################################
 
+#valeys
+s_valey_in='UDDDUDUU'
+valey_ans=countingValleys(s=s_valey_in)
 print("The End")
